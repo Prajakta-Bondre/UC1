4192b63407aeb865f76f7b4a04768f6a185b093e
Prajakta-Bondre <radhabondre20@gmail.com>
2021-09-23 15:15:02 +0530

    changes made

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeUC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Employee = 1;
+            if (Employee == 1)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+    }
+}
